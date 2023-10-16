e755f9b7e0d35c64801786e49bebfda36f9e919f
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2023-10-16 12:48:41 -0700

    click events in components

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,25 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("hello, ninjas", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("mario", e);
+        }}
+      >
+        Click me again
+      </button>
     </div>
   );
 };
 
 export default Home;
-<div className="home">
-  <h2>Homepage</h2>
-</div>;
