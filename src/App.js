238ebfea7ee19c5f2ce41b378a85f0baadc8348a
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2023-10-17 19:17:18 -0700

    React router. But keep in mind we're using v6 as opose to v5 in video

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import Home from "./Home";
 import NavBar from "./Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes as Switch,
+} from "react-router-dom";
+//Switch -> Routes
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/" element={<Home />}></Route>
+            {/* v6 use above. v5 is below */}
+            {/* <Route path="/">
+              <Home/>
+            </Route> */}
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
